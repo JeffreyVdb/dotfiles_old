0600b7e3601942868af52fbc5c0926b933487a33
Jeffrey Vandenborne <jeffrey@vandenborne.co>
2022-04-10 14:31:42 +0200

    chore: install fish and fisher plugin manager

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -5,11 +5,25 @@ is_fedora() {
     [[ "fedora" == $(grep -P '^ID' /etc/os-release | awk -F= '{print $2}') ]]
 }
 
+install_fish_shell() {
+    sudo dnf install -y fish
+    FISHER_SCRIPT=$(mktemp)
+    trap 'rm -f $FISHER_SCRIPT' RETURN
+
+    curl -L -o "$FISHER_SCRIPT" https://raw.githubusercontent.com/jorgebucaran/fisher/4.3.1/functions/fisher.fish
+
+    local CSUM
+    CSUM=$(sha256sum "$FISHER_SCRIPT" | awk '{print $1}')
+    test "34c6f4cb4847d27bd7f081ebeafc10585eea31d3a1b1f7bf630108fc658c9529" == "$CSUM"
+
+    fish "$FISHER_SCRIPT"
+    fish -c "fisher install jorgebucaran/hydro"
+}
+
 if is_fedora; then
     sudo dnf install -y cargo rust
 
-    # Install shell
-    sudo dnf install -y fish
+    install_fish_shell
 
     # Install neovim
     sudo dnf install -y neovim
