daba942bc2b73a65fc783e51ac2162389b21414f
Jeffrey Vandenborne <jeffrey@vandenborne.co>
2022-04-13 00:41:44 +0200

    feat: install vscode

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -33,6 +33,13 @@ install_neovim() {
     fi
 }
 
+install_vscode() {
+    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+    sudo dnf install --refresh -y code
+}
+
 if is_fedora; then
     # Common packages
     sudo dnf install -y fzf nodejs
@@ -48,4 +55,5 @@ if is_fedora; then
 
     install_fish_shell
     install_neovim
+    install_vscode
 fi
